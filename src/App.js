5e74d1a05443841c3e13bc11e80d00b57fc6bdba
Andreas Link <mail@linkandreas.de>
2020-04-15 14:27:32 +0200

    Implement site navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import NavBar from "./components/NavBar.js";
+import Routes from "./routes/routes.js";
+import Footer from "./components/Footer.js";
+
+import "./styles/App.css";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <div className="appContainer">
+        <NavBar />
+        <Routes />
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Made by Link{" "}
+        <NavLink className="inline timityOrange" to="/about">
+          Andreas Link
+        </NavLink>{" "}
+        in Karlsruhe
+      </p>
+    </footer>
+  );
+}
