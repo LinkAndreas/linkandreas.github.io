92e7f4fceca2da97b911af373aaff433af77f4e1
Andreas Link <mail@linkandreas.de>
2022-11-13 08:24:22 +0100

    Support shell script syntax highlighting

--- src/components/CodeRenderer.js
+++ src/components/CodeRenderer.js
@@ -1,15 +1,20 @@
 import React from "react";
-import hljs from "highlight.js";
+import hljs from 'highlight.js';
+import swift from 'highlight.js/lib/languages/swift';
+import bash from 'highlight.js/lib/languages/bash';
 import "../styles/Code.css";
-import swift from "highlight.js/lib/languages/swift";
 
-export default function CodeRenderer({node, inline, className, children, ...props}) {
-  const content = String(children).replace(/\n$/, '')
+export default function CodeRenderer({ node, inline, className, children, ...props }) {
   hljs.registerLanguage('swift', swift)
-  const highlighted = hljs.highlight('swift', content)
-  return !inline ? (
+  hljs.registerLanguage('bash', bash)
+
+  const code = String(children).replace(/\n$/, '')
+  const match = /language-(\w+)/.exec(className || '')
+  const language = match !== null ? match[1] : null
+
+  return (!inline && language !== null) ? (
     <pre className="hljs">
-      <code dangerouslySetInnerHTML={{ __html: highlighted.value }} />
+      <code dangerouslySetInnerHTML={{ __html: hljs.highlight(code, { 'language': language }).value }} />
     </pre>
   ) : (
     <code className={className} {...props}>
