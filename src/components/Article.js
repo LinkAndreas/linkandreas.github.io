713c9510ff851e6c3e66caf7c15af7535edadb8a
Andreas Link <mail@linkandreas.de>
2023-04-15 14:21:43 +0200

    Extract `ArticleData`

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,46 +1,36 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { articles } from "./ArticleData.js";
 import ReactMarkdown from "react-markdown";
 import CodeRenderer from "./CodeRenderer.js";
+import Metadata from "./Metadata.js";
 import ImageRenderer from "./ImageRenderer.js";
 import ParagraphRenderer from "./ParagraphRenderer.js";
-import article1 from "../../assets/articles/09_03_2021/article.md";
-import article2 from "../../assets/articles/16_03_2022/article.md";
-import article3 from "../../assets/articles/13_11_2022/article.md";
 import "../styles/Article.css";
 
 export default function Article() {
-  let { date } = useParams();
-
-  let children = (() => {
-    switch (date) {
-      case "13_11_2022":
-        return article3;
-
-      case "16_03_2022":
-        return article2;
-
-      case "09_03_2021":
-        return article1;
-
-      default:
-        return null;
-    }
-  })();
+  let { id } = useParams();
+  let article = articles.find(article => article.id === id);
 
   return (
     <div className="articleContainer">
-      {children != null ? (
-        <ReactMarkdown
-          children={children}
-          components={{
-            code: CodeRenderer,
-            img: ImageRenderer,
-            p: ParagraphRenderer,
-          }}
+      {article != null ? (
+        <Metadata
+          title={article.title}
+          description={article.description}
+          children={
+            <ReactMarkdown
+              children={article.markdown}
+              components={{
+                code: CodeRenderer,
+                img: ImageRenderer,
+                p: ParagraphRenderer,
+              }}
+            />
+          }
         />
       ) : (
-        <h1>Article not found :(</h1>
+        <h1>Article not found.</h1>
       )}
     </div>
   );
