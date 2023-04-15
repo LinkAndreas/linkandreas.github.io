de195ade20de8b73013d0233c5bfdf97bedbb1b8
Andreas Link <mail@linkandreas.de>
2023-04-15 14:11:41 +0200

    Include `Metadata` Component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as ReactDOMClient from 'react-dom/client';
 import { HashRouter } from "react-router-dom";
+import { HelmetProvider } from 'react-helmet-async';
 
 import NavBar from "./components/NavBar.js";
 import Content from "./components/Content.js";
@@ -10,15 +11,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.css";
 import "./styles/App.css";
 
+const helmetContext = {};
 const App = () => {
   return (
-    <HashRouter className="appRouter" forceRefresh={false}>
-      <div className="appContainer">
-        <NavBar />
-        <Content />
-        <Footer />
-      </div>
-    </HashRouter>
+    <HelmetProvider helmetContext={helmetContext}>
+      <HashRouter className="appRouter" forceRefresh={false}>
+        <div className="appContainer">
+          <NavBar />
+          <Content />
+          <Footer />
+        </div>
+      </HashRouter>
+    </HelmetProvider>
   );
 };
 
