77fbe519517ca2b6d03c2e776889ec1f1dccc5ba
Andreas Link <mail@linkandreas.de>
2023-04-16 08:38:56 +0200

    Remove article reversal

--- src/components/ArticleData.js
+++ src/components/ArticleData.js
@@ -1,22 +1,45 @@
-import article1 from "../../assets/articles/09_03_2021/article.md";
-import article2 from "../../assets/articles/16_03_2022/article.md";
+import article5 from "../../assets/articles/16_04_2023/article.md";
+import article4 from "../../assets/articles/15_04_2023/article.md";
 import article3 from "../../assets/articles/13_11_2022/article.md";
+import article2 from "../../assets/articles/16_03_2022/article.md";
+import article1 from "../../assets/articles/09_03_2021/article.md";
 
 const articles = [
     {
-        id: '09_03_2021',
-        date: '9th of March 2021',
-        markdown: article1,
-        title: 'Building a native macOS app using SwiftUI and Combine',
-        description: `Have you ever been asked to put together the list of licenses 
-        of all frameworks that are used within your iOS, iPad OS, or
-        macOS app? Manually completing this task quickly becomes
-        tedious but may be required due to legal- or customer
-        requests.
-
-        To mitigate this issue, I developed Licenses, a native macOS
-        app that automates this procedure by collecting and exporting
-        your licenses into a single spreadsheet (CSV) file.`
+        id: '16_04_2023',
+        date: '16th of April 2023',
+        markdown: article5,
+        title: 'Automating your iOS App Development Workflow: Continuous Deployment with GitHub Actions',
+        description: `In agile software development, continuous deployment is key to collect 
+        user feedback leading to more reliable and successful iOS apps. Still, deploying to 
+        AppStore Connect is challenging due to signing certificates, provisioning profiles, 
+        and build numbers. In this article, we'll explore how to automate this process, allowing 
+        you to release your apps with a single button press.`
+    },
+    {
+        id: '15_04_2023',
+        date: '15th of April 2023',
+        markdown: article4,
+        title: 'Automating your iOS App Development Workflow: Continuous Testing with GitHub Actions',
+        description: `Are you striving to iterate quickly to deliver new features,
+        all while ensuring the reliability and performance of your iOS application? 
+        Efficient workflows are key to achiving this goal. In this article, we'll 
+        explore how we can leverage the power of Github Actions to automate testing 
+        of iOS applications.`
+    },
+    {
+        id: '13_11_2022',
+        date: '13th of November 2022',
+        markdown: article3,
+        title: 'Story Numbers in Commit Messages: Leveraging the power of Git Hooks',
+        description: `Especially in large software projects with many developers involved,
+        it is best practice to include the story number from the ticket system
+        like Jira or Azure DevOps in every commit. This will let you refer to
+        the original requirements quickly and see how the team thought about the
+        feature, bug fix or release when it was developed. Still, manually adding
+        the number to every commit is cumbersome. Hence, today, we will learn how
+        to automatically derive the story number from the branch name and automate
+        that procedure using Git Commit Hooks.`
     },
     {
         id: '16_03_2022',
@@ -38,18 +61,19 @@ const articles = [
         features rather than writing repetitive error-prone code.`
     },
     {
-        id: '13_11_2022',
-        date: '13th of November 2022',
-        markdown: article3,
-        title: 'Story Numbers in Commit Messages: Leveraging the power of Git Hooks',
-        description: `Especially in large software projects with many developers involved,
-        it is best practice to include the story number from the ticket system
-        like Jira or Azure DevOps in every commit. This will let you refer to
-        the original requirements quickly and see how the team thought about the
-        feature, bug fix or release when it was developed. Still, manually adding
-        the number to every commit is cumbersome. Hence, today, we will learn how
-        to automatically derive the story number from the branch name and automate
-        that procedure using Git Commit Hooks.`
+        id: '09_03_2021',
+        date: '9th of March 2021',
+        markdown: article1,
+        title: 'Building a native macOS app using SwiftUI and Combine',
+        description: `Have you ever been asked to put together the list of licenses 
+        of all frameworks that are used within your iOS, iPad OS, or
+        macOS app? Manually completing this task quickly becomes
+        tedious but may be required due to legal- or customer
+        requests.
+
+        To mitigate this issue, I developed Licenses, a native macOS
+        app that automates this procedure by collecting and exporting
+        your licenses into a single spreadsheet (CSV) file.`
     }
 ];
 
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -15,7 +15,7 @@ export default function Articles() {
           <Routes>
             <Route path="" element={
               <div>
-                {articles.reverse().map((article, index) => (
+                {articles.map((article, index) => (
                   <ArticleItem
                     key={index}
                     id={article.id}
@@ -23,9 +23,12 @@ export default function Articles() {
                     title={article.title}
                     body={
                       <div>
-                        {article.description.split("\n\n").map((line, index) => (
-                          <p key={index}>{line}</p>
-                        ))}
+                        {
+                          article.description
+                            .split("\n\n")
+                            .map((line, index) => (
+                              <p key={index}>{line}</p>
+                            ))}
                       </div>
                     }
                   />
