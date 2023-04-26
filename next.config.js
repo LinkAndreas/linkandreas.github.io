5dcc32e0163653dcfacb186467edaaf356c20566
Andreas Link <mail@linkandreas.de>
2023-04-26 21:02:07 +0200

    Introduce `next.config.js`

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const CnameWebpackPlugin = require('cname-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    });
+    config.plugins.push(
+      new CnameWebpackPlugin({
+        domain: "www.linkandreas.de",
+      }),
+    );
+    return config;
+  }
+};
