598340da56b3b7b3546ed47cf47c75bd9032a04f
Andreas Link <mail@linkandreas.de>
2025-12-31 18:35:11 +0100

    Use client-side rendering in `PrivacyPolicy` component

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -1,18 +1,25 @@
+'use client';
+
 import React from "react";
-import Script from 'next/script'
+import { useEffect, useState } from 'react';
+import Script from 'next/script';
 
 export default function PrivacyPolicy() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
   return (
-    <div>
-      <Script id="show-banner" strategy="beforeInteractive">
-        {`(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);`}
-      </Script>
+    <>
+      <Script src="https://cdn.iubenda.com/iubenda.js" />
       <a
         href="https://www.iubenda.com/privacy-policy/86928215"
-        className="iubenda-black no-brand iubenda-noiframe iubenda-embed iubenda-noiframe "
+        className="iubenda-black no-brand iubenda-noiframe iubenda-embed"
       >
         Privacy Policy
       </a>
-    </div>
+    </>
   );
 }
